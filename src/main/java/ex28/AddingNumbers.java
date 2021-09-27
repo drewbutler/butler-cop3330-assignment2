a0ba7f75db13a239df42c1fad10141405dca36d2
drewbutler <75517767+drewbutler@users.noreply.github.com>
2021-09-27 19:31:41 -0400

    Assignment 28 finished

--- src/main/java/ex28/AddingNumbers.java
+++ src/main/java/ex28/AddingNumbers.java
@@ -4,6 +4,25 @@
  */
 package ex28;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class AddingNumbers {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] x = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter a number: ");
+            x[i] = in.nextInt();
+            in.nextLine();
+        }
+        int total = sumOfX(x);
+        System.out.println("The total is " + total + ".");
+    }
+
+    public static int sumOfX(int[] x) {
+        return Arrays.stream(x).sum();
+    }
 
 }
